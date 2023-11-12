c88a3af4c9b9417007a02503ea9201df0f8c93c3
Nicklauscott <nicklauscott@gmail.com>
2023-11-12 22:00:51 +0200

    Working version 1

---
+++ src/main/kotlin/com/example/SockectRoute.kt
@@ -0,0 +1,43 @@
+package com.example
+
+import com.example.model.MakeTurn
+import com.example.model.TicTacToeGame
+import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.serialization.json.Json
+
+fun Route.socket(game: TicTacToeGame) {
+    route("/play") {
+        webSocket {
+            val player = game.connectPlayer(this)
+            if (player == null) {
+                close(CloseReason(CloseReason.Codes.CANNOT_ACCEPT,
+                    "Two player already connected"))
+                return@webSocket
+            }
+
+            try {
+                incoming.consumeEach { frame ->
+                    if (frame is Frame.Text) {
+                        val action = extractAction(frame.readText())
+                        game.finishTurn(player, action.x, action.y)
+                    }
+                }
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+            } finally {
+                game.disconnectPlayer(player)
+            }
+        }
+    }
+}
+
+private fun extractAction(message: String): MakeTurn {
+    val type = message.substringBefore("#")
+    val body = message.substringAfter("#")
+    return if (type == "make_turn") {
+        Json.decodeFromString(body)
+    }else MakeTurn(-1, -1)
+}
